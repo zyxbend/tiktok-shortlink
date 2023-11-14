ceaa5188232e5f3332afff021d2378fe86684642
zyxbend <zhengyx@nuaa.edu.cn>
2023-11-14 14:25:22 +0800

    修改pom配置

---
+++ admin/src/main/java/com/pddbend/shortlink/admin/ShortlinkAdminApplication.java
@@ -0,0 +1,18 @@
+package com.pddbend.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: pddbend
+ * @Date: 2023-11-13-下午2:50
+ * @Description: 后管系统主启动类
+ */
+
+@SpringBootApplication
+public class ShortlinkAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortlinkAdminApplication.class,args);
+    }
+}

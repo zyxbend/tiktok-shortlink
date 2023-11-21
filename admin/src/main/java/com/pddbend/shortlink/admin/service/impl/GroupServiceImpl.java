f24f452fc89c0145811c3a7cd6f4fc4f281a47e7
zyxbend <zhengyx@nuaa.edu.cn>
2023-11-21 14:50:08 +0800

    feature: 开发短链接后管短链接分组绑定用户

--- admin/src/main/java/com/pddbend/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/pddbend/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,14 +1,19 @@
 package com.pddbend.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.pddbend.shortlink.admin.common.biz.user.UserContext;
 import com.pddbend.shortlink.admin.dao.entity.GroupDO;
 import com.pddbend.shortlink.admin.dao.mapper.GroupMapper;
+import com.pddbend.shortlink.admin.dto.resp.GroupRespDTO;
 import com.pddbend.shortlink.admin.service.GroupService;
 import com.pddbend.shortlink.admin.toolkit.RandomGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Author: pddbend
@@ -34,10 +39,26 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         GroupDO groupDO = GroupDO.builder()
                 .gid(RandomGenerator.generatorRandom())
                 .name(groupName)
+                .username(UserContext.getUsername())
                 .sortOrder(0)
                 .build();
         baseMapper.insert(groupDO);
     }
+
+    /**
+     * 查询用户所有短链接分组
+     * @return 用户短链接分组列表
+     */
+    @Override
+    public List<GroupRespDTO> listGroup() {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
+        List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+        return BeanUtil.copyToList(groupDOList, GroupRespDTO.class);
+    }
+
 
     /**
      * 判断gid是否存在
@@ -47,7 +68,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
     private boolean hasGid(String gid) {
         LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getGid, gid)
-                .eq(GroupDO::getUsername, null);
+                .eq(GroupDO::getUsername, UserContext.getUsername());
         GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
         return hasGroupFlag == null;
     }

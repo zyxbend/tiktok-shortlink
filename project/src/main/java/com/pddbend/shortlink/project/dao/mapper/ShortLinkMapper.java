2d0a932d7e319761fb7858ea4c8c2c9b6e28c677
zyxbend <zyx_bend@163.com>
2023-12-04 15:29:41 +0800

    feature：开发短链接监控（历史访问统计）

--- project/src/main/java/com/pddbend/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/com/pddbend/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.pddbend.shortlink.project.dao.entity.ShortLinkDO;
 import com.pddbend.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 /**
  * @Author: pddbend
@@ -15,4 +17,16 @@ public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
      * 分页统计短链接
      */
     IPage<ShortLinkDO> pageLink(ShortLinkPageReqDTO requestParam);
+
+    /**
+     * 短链接访问统计自增
+     */
+    @Update("update t_link set total_pv = total_pv + #{totalPv}, total_uv = total_uv + #{totalUv}, total_uip = total_uip + #{totalUip} where gid = #{gid} and full_short_url = #{fullShortUrl}")
+    void incrementStats(
+            @Param("gid") String gid,
+            @Param("fullShortUrl") String fullShortUrl,
+            @Param("totalPv") Integer totalPv,
+            @Param("totalUv") Integer totalUv,
+            @Param("totalUip") Integer totalUip
+    );
 }

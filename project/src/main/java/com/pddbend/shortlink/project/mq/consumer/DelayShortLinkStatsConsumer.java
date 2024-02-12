7370575e45adefaea780a5f0295db2f05bd55446
zyxbend <zyx_bend@163.com>
2024-02-12 19:28:40 +0800

    feature: 短链接延迟队列监控消费者幂等逻辑处理

--- project/src/main/java/com/pddbend/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
+++ project/src/main/java/com/pddbend/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
@@ -1,8 +1,11 @@
 package com.pddbend.shortlink.project.mq.consumer;
 
+import com.pddbend.shortlink.project.common.convention.exception.ServiceException;
 import com.pddbend.shortlink.project.dto.biz.ShortLinkStatsRecordDTO;
+import com.pddbend.shortlink.project.mq.idempotent.MessageQueueIdempotentHandler;
 import com.pddbend.shortlink.project.service.ShortLinkService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBlockingDeque;
 import org.redisson.api.RDelayedQueue;
 import org.redisson.api.RedissonClient;
@@ -14,17 +17,20 @@ import java.util.concurrent.locks.LockSupport;
 
 import static com.pddbend.shortlink.project.common.constant.RedisKeyConstant.DELAY_QUEUE_STATS_KEY;
 
+
 /**
  * @Author: pddbend
  * @Date: 2024-02-05-20:43
  * @Description: 延迟记录短链接统计组件
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DelayShortLinkStatsConsumer implements InitializingBean {
 
     private final RedissonClient redissonClient;
     private final ShortLinkService shortLinkService;
+    private final MessageQueueIdempotentHandler messageQueueIdempotentHandler;
 
     public void onMessage() {
         Executors.newSingleThreadExecutor(
@@ -41,7 +47,20 @@ public class DelayShortLinkStatsConsumer implements InitializingBean {
                         try {
                             ShortLinkStatsRecordDTO statsRecord = delayedQueue.poll();
                             if (statsRecord != null) {
-                                shortLinkService.shortLinkStats(null, null, statsRecord);
+                                if (!messageQueueIdempotentHandler.isMessageProcessed(statsRecord.getKeys())) {
+                                    // 判断当前的这个消息流程是否执行完成
+                                    if (messageQueueIdempotentHandler.isAccomplish(statsRecord.getKeys())) {
+                                        return;
+                                    }
+                                    throw new ServiceException("消息未完成流程，需要消息队列重试");
+                                }
+                                try {
+                                    shortLinkService.shortLinkStats(null, null, statsRecord);
+                                } catch (Throwable ex) {
+                                    messageQueueIdempotentHandler.delMessageProcessed(statsRecord.getKeys());
+                                    log.error("延迟记录短链接监控消费异常", ex);
+                                }
+                                messageQueueIdempotentHandler.setAccomplish(statsRecord.getKeys());
                                 continue;
                             }
                             LockSupport.parkUntil(500);
--- project/src/main/java/com/pddbend/shortlink/project/mq/producer/DelayShortLinkStatsProducer.java
+++ project/src/main/java/com/pddbend/shortlink/project/mq/producer/DelayShortLinkStatsProducer.java
@@ -1,5 +1,6 @@
 package com.pddbend.shortlink.project.mq.producer;
 
+import cn.hutool.core.lang.UUID;
 import com.pddbend.shortlink.project.dto.biz.ShortLinkStatsRecordDTO;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBlockingDeque;
@@ -28,6 +29,7 @@ public class DelayShortLinkStatsProducer {
      * @param statsRecord 短链接统计实体参数
      */
     public void send(ShortLinkStatsRecordDTO statsRecord) {
+        statsRecord.setKeys(UUID.fastUUID().toString());
         RBlockingDeque<ShortLinkStatsRecordDTO> blockingDeque = redissonClient.getBlockingDeque(DELAY_QUEUE_STATS_KEY);
         RDelayedQueue<ShortLinkStatsRecordDTO> delayedQueue = redissonClient.getDelayedQueue(blockingDeque);
         delayedQueue.offer(statsRecord, 5, TimeUnit.SECONDS);

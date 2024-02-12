4a803634fdb827340ca70e9087fdfc876d53a6c0
zyxbend <zyx_bend@163.com>
2024-02-12 19:40:36 +0800

    optimize: 初始化 Redis Stream Topic 和消费者组

---
+++ project/src/main/java/com/pddbend/shortlink/project/initialize/ShortLinkStatsStreamInitializeTask.java
@@ -0,0 +1,27 @@
+package com.pddbend.shortlink.project.initialize;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import static com.pddbend.shortlink.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_GROUP_KEY;
+import static com.pddbend.shortlink.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_TOPIC_KEY;
+/**
+ * @Author: pddbend
+ * @Date: 2024-02-12-19:39
+ * @Description: 初始化短链接监控消息队列消费者组
+ */
+@Component
+@RequiredArgsConstructor
+public class ShortLinkStatsStreamInitializeTask implements InitializingBean {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Boolean hasKey = stringRedisTemplate.hasKey(SHORT_LINK_STATS_STREAM_TOPIC_KEY);
+        if (hasKey == null || !hasKey) {
+            stringRedisTemplate.opsForStream().createGroup(SHORT_LINK_STATS_STREAM_TOPIC_KEY, SHORT_LINK_STATS_STREAM_GROUP_KEY);
+        }
+    }
+}
